2fb3ad311ef0fec4f84f5806120a16f9dcef1ccb
dsdeloar02 <dsdeloar02@gmail.com>
2022-10-25 17:56:10 +0600

    cart json added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors())
 
 const categories = require('./data/categories.json')
 const courses = require('./data/courses.json')
+const carts = require('./data/cart.json')
 
 app.get('/', (req, res) => {
     res.send('Api is Running');
@@ -17,10 +18,31 @@ app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '07'){
+        res.send(courses);
+    }
+    else{        
+        const category_news = courses.find(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
-    const singleCours = courses.find(p => p.category_id === id);
+    const singleCours = carts.find(p => p.category_id === id);
     res.send(singleCours);
+})
+
+app.get('/cart/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCart = carts.find(p => p.id === id);
+    res.send(singleCart);
 })
 
 app.listen(port, () => {
